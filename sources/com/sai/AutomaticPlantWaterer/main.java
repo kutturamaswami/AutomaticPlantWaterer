2552ea665b788f26ea204926444a9be272e83385
Kutturamaswami <egsai28@gmail.com>
2019-02-21 19:19:44 -0500

    Test 1

--- sources/com/sai/AutomaticPlantWaterer/main.java
+++ sources/com/sai/AutomaticPlantWaterer/main.java
@@ -129,7 +129,7 @@ public class main {
     }
 
     private static long convertToTime(int mLs) {
-        return 15000; //return time valve needs to be open to deliver certain amount of water, obtained empirically
+        return 16000; //return time valve needs to be open to deliver certain amount of water, obtained empirically
     }
 
     private static void changeValveState(Plant plant, boolean state) {
